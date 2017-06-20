88e798d348822c032e881f72878f0f64ac6e33bb
Wenlong Wang <simbawwl.163.com>
2017-06-20 08:42:20 +0800

    dd_hist_updated

--- matlab/DD_hist/dd_hist.m
+++ matlab/DD_hist/dd_hist.m
@@ -1,32 +1,41 @@
 clc;close all;clear all;
-%fid=fopen('vp_dsp.bin','r');
-fid=fopen('theta_dsp.bin','r');
+fid=fopen('vp_dsp.bin','r');
+%fid=fopen('theta_dsp.bin','r');
 
 nx=2100;
 nz=1801;
 vp=fread(fid,[nz,nx],'float32');
-x1=0:6.25:(nx-1)*6.25/1000.;
-z1=0:6.25:(nz-1)*6.25/1000;
+x1=0:0.01:(nz-1)*10/1000;
+z1=0:0.01:(nx-1)*10/1000;
 
-figure(1);
+hFig1=figure(1);
 imagesc(z1,x1,vp);colormap('gray');
 xlabel('Position (km)');
 ylabel('Depth (km)');
+bound=60;
+caxis([1500,4500]);
+%caxis([-bound,bound]);
+set(gca,'FontSize',14,'FontName','Arial','fontWeight','normal')
+set(hFig1, 'Position', [0 0 700 600]);
+
 vp1=reshape(vp,nx*nz,1);
 
-bound=50;
-%val=linspace(1500,4600,10);
-val=linspace(-bound,bound,11);
+val=linspace(1500,4600,10);
+%val=linspace(-bound,bound,15);
 
-figure(2);
-%hist(vp1(vp1>1500 & vp1<4500),val,1);
-hist(vp1(vp1>-bound &  vp1<bound),val,1);
+hFig2=figure(2);
+hist(vp1(vp1>1500 & vp1<4500),val,1);
+%hist(vp1(vp1>-bound &  vp1<bound),val,1);
 
-xlabel('vp (m/s)');
+xlabel('binned vp_0 (m/s)');
+%xlabel('binned tilt angle (degrees)');
+
 ylabel('count');
 set(get(gca,'child'),'FaceColor','none','EdgeColor','k');
-%xlim([1300 4500]);
-xlim([-45 45]);
+xlim([1300 4500]);
+%xlim([-bound+5 bound-5]);
+set(gca,'FontSize',14,'FontName','Arial','fontWeight','normal')
+set(hFig2, 'Position', [0 0 800 600]);
 
 xsec=4;
 nx1=floor(nx/xsec);
@@ -44,19 +53,20 @@ subvp(:,:,(izsec-1)*xsec+ixsec)=vp((izsec-1)*nz1+1:izsec*nz1,(ixsec-1)*nx1+1:ixs
 end
 end
 
-figure(3);
+hFig3=figure(3);
 for ixsec=1:xsec*zsec
 subplot(xsec,zsec,ixsec);
 subvp1=reshape(subvp(:,:,ixsec),nx1*nz1,1);
-%hist(subvp1(subvp1>1500 & subvp1<4500),val,1);
-hist(subvp1(subvp1>-bound & subvp1<bound),val,1);
+hist(subvp1(subvp1>1500 & subvp1<4500),val,1);
+%hist(subvp1(subvp1>-bound & subvp1<bound),val,1);
 
 %xlabel('vp (mk/s)');
 %ylabel('frequency');
 set(get(gca,'child'),'FaceColor','none','EdgeColor','k');
-%xlim([1300 4500]);
-xlim([-45 45]);
-
+xlim([1300 4500]);
+%xlim([-bound+5 bound-5]);
+set(gca,'FontSize',14,'FontName','Arial','fontWeight','normal')
 end
+set(hFig3, 'Position', [0 0 800 600]);
 
 
