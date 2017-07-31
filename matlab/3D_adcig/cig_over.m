aacfa55e9beb88ef4d6fda396e69bc09232cddd7
Wenlong Wang <wenlong.wang@hit.edu.cn>
2017-07-31 16:05:59 +0800

    matlab cig_over corrected

--- matlab/3D_adcig/cig_over.m
+++ matlab/3D_adcig/cig_over.m
@@ -1,35 +1,37 @@
 clear all; clc;close all;
 
-ng=61;naz=73;
-nz=140;
-ng_stp=1;naz_stp=10;
+ng=61;
+naz=73;
+
+%nz=140;
+nz=100;
+
+ng_stp=1;
+naz_stp=5;
+
 %clb_p=0.8;
 %clb_s=0.3;
 
-clb_p=5e-1;
-clb_s=5e-1;
+clb_p=2e-1;
+clb_s=1e-2;
 
 nbin=6;
 
 flow=0.015;
 fhigh=0.075;
-%fid1=fopen('./vctext/cig_p_141_141','r');
-%fid1=fopen('./vctext/cig_p_141_151','r');
-fid1=fopen('./vctext/cig_p_141_131','r');
-
-
-
-%fid1=fopen('./vctcrsc/cig_p_151_151','r');
-%fid1=fopen('./layer/rflexap/cig_p_50_50','r');
+%fid1=fopen('./vctext/cig_p_141_131','r');
+%fid1=fopen('./cig_p_75_75','r');
+fid1=fopen('d:/3D_elastic_vector_rtm/data/layer/cig_p_50_50','r');
 
 x1=fread(fid1,ng*nz*naz,'float32');
+fclose(fid1);
 
-fid2=fopen('./vctext/cig_s_141_131','r');
-%fid2=fopen('./vctcrsc/cig_s_151_151','r');
-%fid2=fopen('./layer/rflexap/cig_s_50_50','r');
-
+%fid2=fopen('./vctext/cig_s_141_131','r');
+%fid2=fopen('./cig_s_75_75','r');
+fid2=fopen('d:/3D_elastic_vector_rtm/data/layer/cig_s_50_50','r');
 
 y1=fread(fid2,ng*nz*naz,'float32');
+fclose(fid2);
 
 x=zeros(naz,ng,nz);
 y=zeros(naz,ng,nz);
@@ -75,7 +77,7 @@ test2=zeros(ng*2-1,nz,nbin);
     end
 
     % smoothing
-sml=5;
+sml=1;
 smli=2;
 x2=test1;
 y2=test2;
@@ -122,8 +124,8 @@ for j=1:2*ng-1
     test1_(j,k)=test1(j,k,q);
     test2_(j,k)=test2(j,k,q);
     end
-    test1_(j,:)=bandpass(test1_(j,:),nz,10,flow,fhigh);
-    test2_(j,:)=bandpass(test2_(j,:),nz,10,flow,fhigh);
+%    test1_(j,:)=bandpass(test1_(j,:),nz,10,flow,fhigh);
+%    test2_(j,:)=bandpass(test2_(j,:),nz,10,flow,fhigh);
 end
 subplot(2,nbin,q)
 imagesc(icd_gle,dpth,test1_');title('PP-ADCIG');caxis([-clb_p, clb_p]);xlabel('Incident angle (degree)');ylabel('Depth (km)');
@@ -148,8 +150,8 @@ test4=zeros(naz,nz);
         test4(j,k)=test4(j,k)+y(j,i,k);
         end
     end
-      test3(j,:)=bandpass(test3(j,:),nz,10,flow,fhigh);
-      test4(j,:)=bandpass(test4(j,:),nz,10,flow,fhigh);
+      %test3(j,:)=bandpass(test3(j,:),nz,10,flow,fhigh);
+      %test4(j,:)=bandpass(test4(j,:),nz,10,flow,fhigh);
     end
 
 azm_gle=linspace(-180,180,naz);
